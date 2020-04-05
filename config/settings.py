ab95c55fd2a886028852a5c2b38c0638e05286c4
kodai.shinkawa <kodai.shinkawa@seg.local>
2020-04-05 19:50:07 +0900

    create myvenv

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nwxobdstd=_s_v(x3i+*k=$+$6pj(7$k1d+f-)3_7m_!c0tyux'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
