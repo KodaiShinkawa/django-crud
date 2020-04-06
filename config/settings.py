a438e9549e767785bf84dcea8b199afcc50553ac
kodai.shinkawa <kodai.shinkawa@seg.local>
2020-04-06 19:55:17 +0900

    change tado.jpg

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nwxobdstd=_s_v(x3i+*k=$+$6pj(7$k1d+f-)3_7m_!c0tyux'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
